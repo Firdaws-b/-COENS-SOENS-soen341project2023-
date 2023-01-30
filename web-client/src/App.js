b4174f9e51970fe43d9e9f6499680df50c18b678
ItsDezi <j_desman@live.concordia.ca>
2023-01-30 15:48:07 -0500

    cleanup and homepage start

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
-function App() {
+const API_URL = '';//FireBase db api key goes here
+
+const App = () => {
+
+  useEffect(() => {
+//should learn this to retrieve data
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>EmployMe</h1>
     </div>
   );
 }
