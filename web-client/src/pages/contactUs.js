c01f9ac4cd952540b17163e7c5c43484748db824
BayanAlsalem <b_alsa@live.concordia.ca>
2023-03-11 22:36:44 -0500

    Add NavBar to ContactUs page + Styled the page

--- web-client/src/pages/contactUs.js
+++ web-client/src/pages/contactUs.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com';
+
+import { Form, Button } from 'react-bootstrap';
+import Background from '../assets/office_char.jpg';
+import NavBar from "../Components/NavBars/welcomePageNavBar";
+
 import StyledContactForm from "../assets/wrappers/StyledContactForm";
 
 const Result = () => {
@@ -35,47 +40,75 @@ export default function ContactUs() {
   }
 
   return (
+    <>
+    <NavBar />
 
-    <div className='container'>
+    <div style={{
+      backgroundImage: `url(${Background})`,
+      backgroundSize: 'cover',
+      height: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center'
+     
+    }}
+  >
+      <h1 style={{ color: 'black', marginBottom: '45rem' }}>Contact Us! We are Here to Listen</h1>
+      
+
       <form onSubmit={sendEmail}>
-        <div className='form-group'>
-          <label htmlFor='name'>Name:</label>
+
+        <div className='form-group' style={{ marginBottom: '1rem' }}>
+          <label htmlFor='name' style={{ fontSize: '1.3rem', color: '#0D6EFD' }}>Name</label>
           <input
             type='text'
             className='form-control'
             id='name'
             name='name'
             required
+
+            placeholder='Enter your full name'
+            style={{ width: '160%', maxWidth: '500px' }}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='email'>Email:</label>
+
+        <div className='form-group' style={{ marginBottom: '1rem' }}>
+          <label htmlFor='email' style={{ fontSize: '1.3rem', color: '#0D6EFD' }}>Email</label>
           <input
             type='email'
             className='form-control'
             id='email'
             name='email'
             required
+
+            placeholder='Enter your email address'
+            style={{ width: '160%', maxWidth: '500px' }}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='subject'>Subject:</label>
+        <div className='form-group'style={{ marginBottom: '1rem' }}>
+          <label htmlFor='subject' style={{ fontSize: '1.3rem', color: '#0D6EFD' }}>Subject</label>
           <input
             type='text'
             className='form-control'
             id='subject'
             name='subject'
             required
+
+            placeholder='Enter the subject'
+            style={{ width: '160%', maxWidth: '500px' }}
           />
         </div>
-        <div className='form-group'>
-          <label htmlFor='message'>Message:</label>
+        <div className='form-group' style={{ marginBottom: '1rem' }}>
+          <label htmlFor='message' style={{ fontSize: '1.3rem', color: '#0D6EFD' }}>Message</label>
           <textarea
             className='form-control'
             id='message'
             name='message'
             rows='6'
             required
+
+            placeholder='500 maximum characters'
+            style={{ width: '160%', maxWidth: '500px' }}
           ></textarea>
         </div>
         <button type='submit' className='btn btn-primary'>
@@ -83,7 +116,8 @@ export default function ContactUs() {
         </button>
         {showResult ? <Result /> : null}
       </form>
-    </div>
-
+      </div>
+  
+      </>
   );
 }
