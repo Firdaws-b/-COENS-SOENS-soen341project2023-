cd4215526877cbe53d0855bbbd72886a1a3ede76
BayanAlsalem <b_alsa@live.concordia.ca>
2023-03-11 20:06:08 -0500

    styled and customized the role selection page

--- web-client/src/pages/roleSelection.js
+++ web-client/src/pages/roleSelection.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 import NavBar from '../Components/NavBars/welcomePageNavBar'
 import { useNavigate} from 'react-router-dom';
+import Background from '../assets/office_char.jpg';
 
 export const RoleSelection = () => {
     const navigate = useNavigate();
@@ -13,14 +14,47 @@ export const RoleSelection = () => {
       const navigateEmployerSignUp = () => {
         navigate('/employer-sign-up');
       };
+
+      const backgroundStyle = {
+        backgroundImage: `url(${Background})`,
+        backgroundSize: 'cover',
+        backgroundColor: 'blue',
+        flexDirection: 'column',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '93.4vh',
+      };
+
+      const textDivStyle = {
+        backgroundColor: 'transparent',
+        color: 'black',
+        textAlign: 'center',
+        fontSize: '2rem',
+        padding: '1rem',
+        border: 'none'
+      };
+
+
+
   return (
     <>
-    <NavBar/>
-    <div>roleSelection</div>
-    <div className='role-select'>
+     <NavBar/>
+     
+     
+     <div style={backgroundStyle}>
+     
+     <div style={textDivStyle}>
+        Please select what describes you the best
+      </div>
+      
+     <div className='role-select'>
         <Button onClick={navigateToUserSignUp}>Job Seeker</Button>
         <Button onClick={navigateEmployerSignUp}>Employer</Button>
     </div>
+   
+    </div>
     </>
+    
   )
 }
