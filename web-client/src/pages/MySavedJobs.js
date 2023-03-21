ec62c0f641041316e3a2128f85139bd2122370cc
Firdaws-b <firdaws.bouzeghaya@mail.concordia.ca>
2023-03-21 00:14:02 -0400

    #96 Code improvement when fetching queries of saved jobs.

--- web-client/src/pages/MySavedJobs.js
+++ web-client/src/pages/MySavedJobs.js
@@ -5,64 +5,52 @@ import { useUserAuth } from '../firebase/UserAuthContext';
 import NavBarProfilePage from '../Components/NavBars/NavBarProfilePage';
 import { auth, firestore } from '../firebase/firebase';
 import Wrapper from "../assets/wrappers/ProfilePageFormPage";
-//import { doc } from "@firebase/firestore";
 import { collection, doc, getDoc, updateDoc } from "firebase/firestore";
 import '../styles.css'
 import { storage } from "../firebase/firebase";
 import JobCardList from "../Components/JobCardList";
 
 const MySavedJobs = () => {
-    // console.log('Rendering MySavedJobs component');
     const { user } = useUserAuth();
     const [savedJobsData, setSavedJobsData] = useState([]);
-    useEffect(() => {
-        const getSavedJobs = async () => {
-            try {
-                //console.log('getSavedJobs called');
-                const userRef = doc(firestore, "Users", user.uid);
-                //console.log('BEFORE USERDOC: ');
-                // const userDoc = await userRef.get();
-                //getDoc(doc(firestore, "Users", uid))
-                const userDoc = await getDoc(doc(firestore, "Users", user.uid));
-                //console.log('AFTER USERDOC: ');
-                if (!userDoc.exists) {
-                    console.log("User document not found !");
-                    return;
-                }
-                //console.log(userRef.path)
-                //console.log('user: ',user);
-                const savedJobsIds = userDoc.data().savedJobs;
-                const newSavedJobsData = [];
-                //const savedJobsData = [];
-                //console.log(savedJobsIds.length)
-                for (let i = 0; i < savedJobsIds.length; i++) {
-                    const jobRef = doc(firestore, "Postings", savedJobsIds[i]);
-                    //getDocs(collection(firestore, "Postings"))
-                    const jobDoc = await getDoc(doc(firestore, "Postings", savedJobsIds[i]));
-                    if (jobDoc.exists) {
-                        const jobData = jobDoc.data();
-                        newSavedJobsData.push({
-                            Job: jobData.Job,
-                            Company: jobData.Company,
-                            CompanyLogo: jobData.CompanyLogo,
-                        });
-                    }
-                }
 
-                setSavedJobsData(newSavedJobsData);
-                //console.log(savedJobsData);
-            } catch (error) {
-                console.log('Error in getSavedJobs:', error);
-            }
-        };
+    useEffect(() => {
         getSavedJobs();
-    }, [user, savedJobsData]);
+    }, []);
+
+    const getSavedJobs = async () => {
+        try {
+            const userRef = doc(firestore, "Users", user.uid);
+            const userDoc = await getDoc(userRef);
+
+            if (!userDoc.exists) {
+                console.log("User document not found !");
+                return;
+            }
+
+            const savedJobsIds = userDoc.data().savedJobs;
+            const jobRefs = savedJobsIds.map(id => doc(firestore, "Postings", id));
+            const jobDocs = await Promise.all(jobRefs.map(getDoc));
+
+            const newSavedJobsData = jobDocs
+                .filter(doc => doc.exists)
+                .map(doc => ({
+                    Job: doc.data().Job,
+                    Company: doc.data().Company,
+                    CompanyLogo: doc.data().CompanyLogo,
+                }));
+
+            setSavedJobsData(newSavedJobsData);
+        } catch (error) {
+            console.log('Error in getSavedJobs:', error);
+        }
+    };
 
     return (
         <>
             <NavBarProfilePage />
             <Wrapper className="mt-4">
-                <h>My Saved Jobs</h>
+                <h1>My Saved Jobs</h1>
                 <div className="job-card-list-container">
                     <JobCardList jobs={savedJobsData} />
                 </div>
@@ -70,4 +58,5 @@ const MySavedJobs = () => {
         </>
     );
 };
+
 export default MySavedJobs;
