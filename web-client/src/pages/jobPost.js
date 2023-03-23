6e4ce48a0f1629dd2c939a98339a0ad4f831c732
Firdaws-b <firdaws.bouzeghaya@mail.concordia.ca>
2023-03-23 17:06:24 -0400

    #99 Bug fix when calling the function where in jobPost

--- web-client/src/pages/jobPost.js
+++ web-client/src/pages/jobPost.js
@@ -3,7 +3,7 @@ import { Button, Form } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import NavBar from '../Components/NavBars/authorizedNavBar';
 import { useUserAuth } from '../firebase/UserAuthContext';
-import { deleteDoc, collection, doc, FieldValue, arrayUnion, updateDoc } from "@firebase/firestore";
+import { deleteDoc, collection, doc, FieldValue, arrayUnion, updateDoc, getDoc } from "@firebase/firestore";
 import { firestore } from '../firebase/firebase';
 import { useNavigate } from 'react-router-dom';
 import NavBarProfilePage from '../Components/NavBars/NavBarProfilePage';
@@ -42,15 +42,17 @@ export const JobPost = () => {
   useEffect(()=>{
     const getSavedJobs = async() => {
       const docRef =doc(firestore,"Users",user.uid);
-      const docSnap = await docRef.get();
+      const docSnap = await getDoc(docRef);
       if (docSnap.exists()){
         const userData = docSnap.data();
         const savedJobsData = [];
-        for (let i = 0; i < userData.savedJobesData.length;i++){
-          const postingRef = doc(firestore,"Postings",userData.savedJobs[i]);
-          const postingSnap = await postingRef.get();
-          if (postingSnap.exists()){
-            savedJobsData.push(postingSnap.data());
+        if(Array.isArray(userData.savedJobsData)){ //check if this is a valid array.
+          for (let i = 0; i < userData.savedJobesData.length;i++){
+            const postingRef = doc(firestore,"Postings",userData.savedJobs[i]);
+            const postingSnap = await postingRef.get();
+            if (postingSnap.exists()){
+              savedJobsData.push(postingSnap.data());
+            }
           }
         }
         setSavedJobs(savedJobsData);
