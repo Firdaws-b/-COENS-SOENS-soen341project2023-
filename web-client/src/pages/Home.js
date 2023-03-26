74734d92440d1c52cb6a7c252244c858a7fc2ca7
Firdaws-b <firdaws.bouzeghaya@mail.concordia.ca>
2023-03-25 21:24:59 -0400

    #35 fixed the styling of Contact us Page

--- web-client/src/pages/Home.js
+++ web-client/src/pages/Home.js
@@ -25,7 +25,6 @@ export default function Home() {
         return (
           <>
             <NavBarProfilePage/>
-            <div>Your job postings</div>
             <ListJobsFromUID />
           </>
         );
